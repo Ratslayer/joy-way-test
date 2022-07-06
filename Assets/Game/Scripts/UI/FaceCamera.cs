7860a7b81fd2dc773d5ddfa7cf91ba41f6ad4327
Arseni <arseni.natapov@gmail.com>
2022-07-06 15:43:07 +0300

    Dummy UI now faces camera at all times.

---
+++ Assets/Game/Scripts/UI/FaceCamera.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Camera _camera;
+    private void Update()
+    {
+        var dir = _camera.transform.position - transform.position;
+        dir.y = 0f;
+        transform.forward = dir;
+    }
+}
