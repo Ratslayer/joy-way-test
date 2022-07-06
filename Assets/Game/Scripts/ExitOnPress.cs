3a2b82f32fd4f41e909e99b7449fc1d12ca3e858
Arseni <arseni.natapov@gmail.com>
2022-07-06 14:16:02 +0300

    Added exit on escape.

---
+++ Assets/Game/Scripts/ExitOnPress.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ExitOnPress : MonoBehaviour
+{
+    [SerializeField]
+    private Key _key;
+    private void Update()
+    {
+        if (Keyboard.current[_key].wasPressedThisFrame)
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;                    
+#else
+            Application.Quit();
+#endif
+    }
+}
