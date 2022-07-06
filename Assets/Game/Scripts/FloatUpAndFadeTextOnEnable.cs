97b90e3d9f381558c8c38f97b74dc141c6869148
Arseni <arseni.natapov@gmail.com>
2022-07-06 12:55:12 +0300

    Added damage numbers ui over enemy head.

---
+++ Assets/Game/Scripts/FloatUpAndFadeTextOnEnable.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+public class FloatUpAndFadeTextOnEnable : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _text;
+    [SerializeField]
+    private float _height = 0.5f, _duration = 0.3f;
+    private float _elapsedTime;
+    private void OnEnable()
+    {
+        _elapsedTime = 0f;
+    }
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+        if (_elapsedTime > _duration)
+            gameObject.SetActive(false);
+        else
+        {
+            var factor = _elapsedTime / _duration;
+            transform.localPosition = Vector3.Lerp(Vector3.zero, Vector3.up * _height, factor);
+            _text.alpha = Mathf.Lerp(1, 0, factor);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/TakeDamageUI.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TakeDamageUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI _textPrefab;
+    [SerializeField]
+    private Health _health;
+    private void OnEnable()
+    {
+        _health.TookDamage += OnDealDamage;
+    }
+    private void OnDisable()
+    {
+        _health.TookDamage -= OnDealDamage;
+    }
+    private void OnDealDamage(float amount, Collision collision)
+    {
+        var text = PoolManager.Instance.Spawn(_textPrefab, transform);
+        text.transform.position = transform.position;
+        text.text = amount.ToString("N0");
+    }
+}
